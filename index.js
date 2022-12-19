368659642bf922eec93f562114736d7109d4d29b
Abalibra <64278817+Abalibra@users.noreply.github.com>
2022-12-19 15:14:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var http = require('http');
+const bodyParser=require('body-parser'); 
+
+app.use(bodyParser.json()) ;
+
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    console.log(req.bodyParser)
+    
     res.end();
 }).listen(process.env.PORT || 3000);
